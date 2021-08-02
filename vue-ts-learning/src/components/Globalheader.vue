f674bb944834e7f84a851a8b02b0ab49d4df4e19
untilYou <wanggang@until-you-possibles-MacBook-Pro.local>
2021-08-02 23:40:20 +0800

    增加header组件

---
+++ vue-ts-learning/src/components/Globalheader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    header
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+export interface UserProps{
+  isLogin : boolean;
+  name?   : string;
+  id?     : number
+
+}
+export default defineComponent({
+  name: "GlobalHeader",
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+
+</script>
